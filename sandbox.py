443020fda8e11914d25cef1e05eebaab4c76bd22
Jivelien <julien.Delannoy@hotmail.com>
2021-01-21 01:05:44 +0100

    start... need a better extrapolation for 2D

--- sandbox.py
+++ sandbox.py
@@ -1,25 +1,36 @@
 import math
 import numpy as np
 from matplotlib import pyplot as plt
-import random
+from random import random
 
-height = 100
-width = 200
+height = 1000
+width = 2000
 canva = np.empty([height,width])
 
-canva[:,:]= 0
-canva[10:20,100:110]= 254
-
-for y in range(height):
-    for x in range(width):
-        canva[y,x] = x + (math.sin(x) + math.sin(y) + 2) * 254/4
-
-plt.imshow(canva)
-
-class perlin:
+class Perlin:
     def __init__(self, size, frequency):
         self.size = size
         self.frequency = frequency
-        self.noise = [0]*size
-        pass
-    
+        self.point = [random() for _ in range(self.frequency)]
+        self.noise = [0]*self.size
+
+        for i in range(self.size):
+            weights = []
+            for j in range(self.frequency):
+                p_pos = self.size // self.frequency * j
+                dist = min(abs(p_pos - i), abs((self.size + p_pos - i) % self.size))
+                w = ((self.size - dist)/self.size )**10
+                weights.append(w)
+            self.noise[i] = sum([weights[k]*self.point[k] for k in range(self.frequency)]) / sum(weights)
+
+p_row = Perlin(height, 500)
+p_col = Perlin(width, 1000)
+
+for r in range(height):
+    for c in range(width):
+        canva[r,c] = p_row.noise[r] + p_col.noise[c]
+
+plt.imshow(canva)
+plt.show()
+
+plt.plot(p_row.noise)
